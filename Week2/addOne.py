53c56b0d0feb6cfd5a3da81e3720132c97a0651d
unknown <G00411297@gmit.ie>
2022-02-03 20:57:43 +0000

    Week2

---
+++ Week2/addOne.py
@@ -0,0 +1,11 @@
+from audioop import add
+from codecs import getincrementaldecoder
+
+from pyparsing import commonHTMLEntity
+
+
+name=input("Sadie")
+print('Hello '+Sadie+'\Nice to meet you')
+#other format
+print('Hello {}\nNice to meet you'.format(Sadie))
+ 
---
+++ Week2/hello2.py
@@ -0,0 +1,5 @@
+name=input("Sadie")
+print('Hello'+Sadie+'\nNice to meet you')
+# other format
+print('Hello {}\nNice to meet you'.format(Sadie))
+
