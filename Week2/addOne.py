69b4f0b529003827977ff8e624324846a65e0a79
unknown <G00411297@gmit.ie>
2022-02-03 22:01:09 +0000

    Week2

--- Week2/addOne.py
+++ Week2/addOne.py
@@ -1,4 +1,6 @@
-name=input("Sadie")
-print('Hello '+Sadie+'\Nice to meet you')
-#other format
-print('Hello {}\nNice to meet you'.format(Sadie))
+# read in a number and add 1
+# Author: Sadie Concannon
+
+number = int(input('please enter a number'))
+newNumber = number + 1
+print ('{} plus one is {}'.format(number, newNumber))
