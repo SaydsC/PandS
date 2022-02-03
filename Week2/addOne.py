40614c177eda7f211151a912ad7d9fd54ffcf599
unknown <G00411297@gmit.ie>
2022-02-03 21:26:08 +0000

    Week2

--- Week2/addOne.py
+++ Week2/addOne.py
@@ -1,11 +1,4 @@
-from audioop import add
-from codecs import getincrementaldecoder
-
-from pyparsing import commonHTMLEntity
-
-
 name=input("Sadie")
 print('Hello '+Sadie+'\Nice to meet you')
 #other format
 print('Hello {}\nNice to meet you'.format(Sadie))
- 
---
+++ Week2/helloWorld.py
@@ -0,0 +1,4 @@
+#helloWorld.py
+# This program just prints out Hello helloWorld
+# author: Sadie Concannon
+print ("HelloWorld")
