7151df5187aab85ab1929f2b59784136782e11b1
SaydsC <97982646+SaydsC@users.noreply.github.com>
2022-03-08 22:37:10 +0000

    Delete addOne.py

--- Week2/addOne.py
+++
@@ -1,6 +0,0 @@
-# read in a number and add 1
-# Author: Sadie Concannon
-
-number = int(input('please enter a number'))
-newNumber = number + 1
-print ('{} plus one is {}'.format(number, newNumber))
