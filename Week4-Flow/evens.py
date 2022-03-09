4c1f543f0801ee951810e1d6d626714ba579acd1
SaydsC <97982646+SaydsC@users.noreply.github.com>
2022-03-09 13:28:30 +0000

    Delete evens.py

--- Week4-Flow/evens.py
+++
@@ -1,9 +0,0 @@
-# program that prints out all even numbers from 2 to 100
-#Author: Sadie Concannon
-
-numberTo = 100
-evenNum = 2
-while evenNum <= numberTo:
-    print (evenNum)
-    evenNum += 2
-    
